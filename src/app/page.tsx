0ab15375681c19e03297ded171c9cd73a16eb91b
salmanahmed221 <salmanahmed121221@gmail.com>
2024-03-07 23:46:39 +0500

    rituals navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,145 @@
-import Image from "next/image";
+"use client";
+import React, { useState } from "react";
+import { IoSearchSharp } from "react-icons/io5";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
+import Link from "next/link";
 
-export default function Home() {
+const NavBar = () => {
+  // State to track active link
+  const [activeLink, setActiveLink] = useState("Treatments");
+  const [nav, setNav] = useState(true);
+
+  // Function to handle click event on links
+  const handleLinkClick = (link: any) => {
+    setActiveLink(link);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="w-full ">
+      <div className=" md:flexjustify-between p-5 md:mx-10 items-center">
+        {/* name */}
+        <div className="flex justify-between items-center">
+          {/* Hamburger icons */}
+          <div
+            className="md:hidden text-2xl z-10"
+            onClick={() => {
+              setNav(!nav);
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {nav ? <GiHamburgerMenu /> : <ImCross />}
+          </div>
+          <p className="font-bold md:text-2xl text-3xl text-center md:text-left">
+            Ritual
+          </p>
+          {/* Search icons */}
+          <div className="md:hidden text-3xl">
+            <IoSearchSharp />
+          </div>
+        </div>
+        {/* nav items */}
+        <div className="  hidden md:flex   ">
+          <div className="flex space-x-3 font-semibold text-sm ">
+            <p
+              className={`cursor-pointer ${
+                activeLink === "Treatments"
+                  ? "bg-[#33364A] px-4   py-2 rounded-3xl text-white"
+                  : "text-black px-4   py-2"
+              }`}
+              onClick={() => handleLinkClick("Treatments")}
+            >
+              Treatments
+            </p>
+            <p
+              className={`cursor-pointer ${
+                activeLink === "Consult"
+                  ? "bg-[#33364A] px-4   py-2 rounded-3xl text-white"
+                  : "text-black px-4   py-2"
+              }`}
+              onClick={() => handleLinkClick("Consult")}
+            >
+              Consult with a doctor
+            </p>
+            <p
+              className={`cursor-pointer ${
+                activeLink === "Learn"
+                  ? "bg-[#33364A] px-4   py-2 rounded-3xl text-white"
+                  : "text-black px-4   py-2"
+              }`}
+              onClick={() => handleLinkClick("Learn")}
+            >
+              Learn
+            </p>
+            <p
+              className={`cursor-pointer ${
+                activeLink === "Contact"
+                  ? "bg-[#33364A] px-4   py-2 rounded-3xl text-white"
+                  : "text-black px-4   py-2"
+              }`}
+              onClick={() => handleLinkClick("Contact")}
+            >
+              Contact us
+            </p>
+            <p
+              className={`cursor-pointer ${
+                activeLink === "About"
+                  ? "bg-[#33364A] px-4   py-2 rounded-3xl text-white"
+                  : "text-black px-4   py-2"
+              }`}
+              onClick={() => handleLinkClick("About")}
+            >
+              About us
+            </p>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {/* Overlay */}
+      {!nav ? (
+        <div className="md:hidden fixed top-0 left-0 h-[100%] bg-white text-black w-[80%] flex flex-col pt-36 pl-7 gap-y-10 font-bold text-3xl ">
+          <Link
+            href={"/"}
+            onClick={() => {
+              setNav(!nav);
+            }}
+          >
+            Treatments
+          </Link>
+          <Link
+            href={"/"}
+            onClick={() => {
+              setNav(!nav);
+            }}
+          >
+            Consult with a doctor
+          </Link>
+          <Link
+            href={"/"}
+            onClick={() => {
+              setNav(!nav);
+            }}
+          >
+            Contact us
+          </Link>
+          <Link
+            href={"/"}
+            onClick={() => {
+              setNav(!nav);
+            }}
+          >
+            Skills
+          </Link>
+          <Link
+            href={"/"}
+            onClick={() => {
+              setNav(!nav);
+            }}
+          >
+            About us
+          </Link>
+        </div>
+      ) : null}
+    </div>
   );
-}
+};
+
+export default NavBar;
